27366e6c6c066b3ca215e740f18063dbf5d06a6d
CHW <seven.nomad@gmail.com>
2025-07-03 16:54:15 +0200

    execute perms

--- Setup/lib/config/zsh.sh
+++ Setup/lib/config/zsh.sh
@@ -6,38 +6,33 @@
 #######--- START OF FILE ---#######
 setup_zsh() {
     print_status "ZSH_CONF" "Setting up ZSH configuration..."
-    
-    # Install oh-my-zsh if not already installed
+
+    # 1. Ensure the ZDOTDIR exists before we do anything else.
+    mkdir -p "$HOME/.config/zsh"
+
+    # 2. Create .zshenv to set ZDOTDIR. This is critical and must happen first.
+    echo 'export ZDOTDIR="$HOME/.config/zsh"' > "$HOME/.zshenv"
+
+    # 3. For safety, remove any old .zshrc from the home directory to avoid conflicts.
+    rm -f "$HOME/.zshrc"
+
+    # 4. Install oh-my-zsh and plugins if they don't exist.
     if [ ! -d "$HOME/.local/share/zsh/oh-my-zsh" ]; then
-        print_status "OMZ_START" "Installing Oh My Zsh..."
+        print_status "OMZ_START" "Installing Oh My Zsh and plugins..."
         export ZSH="$HOME/.local/share/zsh/oh-my-zsh"
-        
-        print_status "OMZ_INST" "Installing Oh My Zsh..."
-        execute_and_log "install_omz" \
-            "Installing Oh My Zsh" \
-            "OMW_INST" || return 1
 
-        # Install zsh plugins
-        print_status "ZSHAUTO" "Cloning autocomplete ..."
-        execute_and_log "zsh_auto" \
-            "Installing zsh-autosuggestions" \
-            "ZSHAUTO" || return 1
-
-        print_status "HGLGT_SYNT" "Cloning Syntax Highlighting ..."
-        execute_and_log "install_omz_syntax" \
-            "Installing zsh-syntax-highlighting" \
-            "HGLGT_SYNT" || return 1
-        
-        print_status "P10K" "Cloning PwerLVL 10K ..."
-        execute_and_log "install_p10k" \
-            "Installing powerlevel10k" \
-            "P10K" || return 1
+        execute_and_log "install_omz" "Installing Oh My Zsh" "OMZ_INST" || return 1
+        execute_and_log "zsh_auto" "Installing zsh-autosuggestions" "ZSHAUTO" || return 1
+        execute_and_log "install_omz_syntax" "Installing zsh-syntax-highlighting" "HGLGT_SYNT" || return 1
+        execute_and_log "install_p10k" "Installing powerlevel10k" "P10K" || return 1
     fi
-    echo "export ZDOTDIR=\"\$HOME/.config/zsh\"" > ~/.zshenv
 
-    # Copy ZSH configurations
+    # 5. Now that ZDOTDIR is set and plugins are installed, write the .zshrc.
     source "$SCRIPT_DIR/lib/config/zxc_zsh.sh"
+
+    print_success "ZSH_CONF" "ZSH configuration complete."
 }
+
 
 
 #######--- END OF FILE ---#######
