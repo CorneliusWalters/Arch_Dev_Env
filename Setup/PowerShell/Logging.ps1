32cc771b668333e180f37ac478eeb12b84fe53a2
CorneliusWalters <45041532+CorneliusWalters@users.noreply.github.com>
2025-07-19 02:28:15 +0200

    Update Logging.ps1
    death seems an easy escape

--- Setup/PowerShell/Logging.ps1
+++ Setup/PowerShell/Logging.ps1
@@ -1,19 +1,17 @@
+# Setup/PowerShell/Logging.ps1
+
 class WslLogger {
     # --- Properties ---
     [string]$LogFile
     [string]$LogDir
 
     # --- Constructor ---
-    # This method runs automatically when we create a new logger object.
     WslLogger([string]$BasePath = "C:\wsl") {
         $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
         $logDir = "$BasePath\tmp\logs\$timestamp"
-
-        # Set the object's own properties using $this
         $this.LogDir = $logDir
         $this.LogFile = "$logDir\powershell_install.log"
 
-        # Create log directories
         $directoriesToCreate = @("$BasePath", "$BasePath\tmp", "$BasePath\tmp\logs", $this.LogDir)
         foreach ($dir in $directoriesToCreate) {
             if (-not (Test-Path $dir)) {
@@ -21,7 +19,6 @@ class WslLogger {
             }
         }
 
-        # Initialize the log file with a header
         "=== PowerShell Installation Log Started at $(Get-Date) ===" | Out-File -FilePath $this.LogFile
         "=== System Information ===" | Out-File -FilePath $this.LogFile -Append
         "User: $env:USERNAME" | Out-File -FilePath $this.LogFile -Append
@@ -32,21 +29,15 @@ class WslLogger {
     }
 
     # --- Methods ---
-    # A method for writing standard log messages.
     [void]WriteLog([string]$Level, [string]$Message, [ConsoleColor]$ForegroundColor = "White") {
         $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
         $logMessage = "[$timestamp] [$Level] $Message"
-
         Write-Host $logMessage -ForegroundColor $ForegroundColor
-
-        # This will now work correctly because $this is a real object instance.
         $logMessage | Out-File -FilePath $this.LogFile -Append
     }
 
-    # A method for writing formatted headers.
     [void]WriteHeader([string]$Message) {
         $separator = "================================================================="
-        # Note: We no longer need the '&' call operator. These are direct method calls.
         $this.WriteLog("HEADER", $separator, "Cyan")
         $this.WriteLog("HEADER", $Message, "Cyan")
         $this.WriteLog("HEADER", $separator, "Cyan")
